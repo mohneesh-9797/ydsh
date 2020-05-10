335197efcf0a059d182f7320458e17893d413eb8
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2020-05-10 17:02:32 +0900

    fix

--- src/type_checker.cpp
+++ src/type_checker.cpp
@@ -1611,7 +1611,7 @@ void TypeChecker::visitSourceListNode(SourceListNode &node) {
         assert(isa<StringNode>(*e) || isa<WildCardNode>(*e));
         if(isa<StringNode>(*e)) {
             auto ref = StringRef(cast<StringNode>(*e).getValue());
-            if(ref.find(StringRef("\0",  1)) != StringRef::npos) {
+            if(ref.find(nullStrRef) != StringRef::npos) {
                 RAISE_TC_ERROR(NullInPath, node.getPathNode());
             }
         }
